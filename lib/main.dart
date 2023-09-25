51e8a57f6951fedf5fa70fb8ccd0b13b2874695c
Ryan Rachic <rrachic33@gmail.com>
2023-09-25 10:32:10 -0400

    Add AppBar, differnet text, colors

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,63 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Testing!!! It works!! Haylee, Ryan, Zoe'),
+        appBar: AppBar(
+          backgroundColor: Colors.green[300],
+          shadowColor: Colors.blue[300],
+          title: Title(
+            color: Colors.black,
+            title: "Testing Page",
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('THIS IS THE MAIN PAGE'),
+                Spacer(flex: 5),
+                Tab(icon: Icon(Icons.airplane_ticket)),
+                Spacer(),
+                Tab(icon: Icon(Icons.airplanemode_active)),
+                Spacer(),
+                Tab(icon: Icon(Icons.alarm)),
+                Spacer(),
+                Text("Welcome to the Brig"),
+              ],
+            ),
+          )
         ),
-      ),
+        body: const Center(
+          child: Column(
+            children: [
+              Text(
+                'Italic style text', 
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+              Text(
+                'Normal text',
+                style: TextStyle(fontStyle: FontStyle.italic)),
+              Text(
+                'Size of text is 30',
+                style: TextStyle(fontSize: 30.0)),
+              Text(
+                'Italic, size 50 and BOLD',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold
+                ),),
+            ],
+          ),
+        ),
+
+        // want to add a bottom sheet of some sort, but confused on 
+        // what onClosing is and how to use it
+
+        /*bottomSheet: BottomSheet(
+          backgroundColor: Colors.yellow[400],
+          shadowColor: Colors.blue[300],
+          onClosing: 
+        )*/
+      )
     );
+
   }
 
   // look at current time, output value
