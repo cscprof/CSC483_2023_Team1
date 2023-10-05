128308198998f108036d51d110e61a3768abc048
amvolpe <amvolpe@geneva.edu>
2023-10-05 13:49:17 -0400

    order page base

---
+++ lib/screens/order.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'OrderPage',
+      home: Center(child: Text('Hello Everyone')),
+      
+    );
+  }
+}
