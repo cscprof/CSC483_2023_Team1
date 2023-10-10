0c6eecdbfecce2e44dd00b37acc3b01867dc41c0
Zoe Pifer <znpifer@geneva.edu>
2023-10-10 14:12:14 -0400

    home page update 10/10/23

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,70 @@
 import 'package:flutter/material.dart';
-
+import 'login.dart'; // Import the Order.dart file
+import 'widgets/headerBar.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'HomePage',
-      home: Center(child: Text('Hello Everyone - Zoe')),
-
+    return MaterialApp(
+      title: 'Mobile Order Page',
+      home: Scaffold(
+        appBar: TopAppBar(AppBar(), "Home Page", Colors.yellow, Colors.black),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome to our mobile order page!',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Display a dialog with two buttons: New Order and Reorder
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Order Options'),
+                        content: const Text('Choose an option:'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              // Close the dialog
+                              Navigator.of(context).pop();
+                              
+                              // Add logic for creating a new order here
+                              // Navigate to a new order screen
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => LoginPage()),
+                              );
+                            },
+                            child: const Text('New Order'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              // Close the dialog
+                              Navigator.of(context).pop();
+                              
+                              // Add logic for reordering here
+                              // For example, repeat the previous order
+                            },
+                            child: const Text('Reorder'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Place an Order'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
