513db7ea8e60e4f2edcc4ae7f26a8236e62cf2da
amvolpe <amvolpe@geneva.edu>
2023-10-10 13:48:14 -0400

    order page changes

--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -5,8 +5,48 @@ class OrderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'OrderPage',
-      home: Center(child: Text('Hello Everyone')),
-      
+      home: const OrderPage(),
     );
   }
+}
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  _OrderPageState createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  final List<String> items = [
+    'Entree', 
+    'Sides',
+    'Fruit',
+    'Dessert',
+    'Drinks'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Start New Order'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context) {
+          return ListTile(
+            leading: Icon(
+              Icons.circle,
+            ),
+            title: Text(items[index]),
+            // subtitle: Text(''),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {}
+          )
+        }
+      )
+    )
+  }
+
 }
