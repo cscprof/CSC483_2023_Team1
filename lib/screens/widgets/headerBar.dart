e15fc16c56448e32ae0dbe04fcf27c982334a4ad
Ryan Rachic <rrachic33@gmail.com>
2023-10-11 14:16:48 -0400

    headerBar: center text, bold

--- lib/screens/widgets/headerBar.dart
+++ lib/screens/widgets/headerBar.dart
@@ -1,3 +1,5 @@
+
+import 'package:brig_project/screens/login.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,18 +14,33 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      actions: [
-          const Spacer(),
-          Text(textInput, selectionColor: Colors.black,),
-          const Spacer(),
-          const Icon(Icons.mouse),
-          const Spacer(),
-          
-        ],
-        title: Text(textInput),  
-        shape: ShapeBorder.lerp(null, null, 0.2),
-        backgroundColor: backgroundColor,
-        shadowColor: shadowColor,
+      // actions: [
+
+      // ],
+      titleTextStyle: const TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+      leading: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(builder: (context) => LoginPage())
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          elevation: 12.0,
+          textStyle: const TextStyle(color: Colors.white),
+          backgroundColor: backgroundColor
+        ),
+        child: const Icon(Icons.settings)
+      ),
+      title: Text(textInput),  
+      centerTitle: true,
+      backgroundColor: backgroundColor,
+      shadowColor: shadowColor, 
+      //bottom: 
     );
   }
 
