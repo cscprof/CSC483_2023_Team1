bd6ec6fbe2734b7bdd4110085fc84c1f14f360d1
Haylee <hpollock3@gmail.com>
2023-10-12 13:49:57 -0400

    change Haylee

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,13 +1,82 @@
 import 'package:flutter/material.dart';
+import 'widgets/headerBar.dart';
 
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'LoginPage',
-      home: Center(child: Text('Hello Everyone')),
-
+    return MaterialApp( 
+      title:"Login Page",
+       home:Scaffold(
+        appBar: TopAppBar(AppBar(), "Login Page", Colors.yellow, Colors.black),
+         body: Center(
+           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign In With Geneva Student ID:',
+                style: TextStyle(fontSize: 30),
+              ),
+            
+             const SizedBox(
+              width: 250,
+             child: TextField(
+             obscureText: true,
+             decoration: InputDecoration(
+             border: OutlineInputBorder(),
+             labelText: 'User ID:',
+                  ),
+                 ),
+               ),
+                const SizedBox(
+              width: 250,
+             child: TextField(
+             obscureText: true,
+             decoration: InputDecoration(
+             border: OutlineInputBorder(),
+             labelText: 'PIN:',
+                  ),
+                 ),
+               ),
+              ElevatedButton(
+               style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.yellow),
+                ),
+                child: const Text('Sign In'),
+               onPressed: ()
+                {
+                },
+              ),      
+              ElevatedButton(
+               style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.yellow),
+                ),
+                child: const Text('Lost ID Card?'),
+               onPressed: ()
+                {
+                },
+              ),
+            ],  
+          ),
+        ),
+      )
     );
+
   }
+
+  // look at current time, output value
+
+  DateTime currentTime() {
+    return DateTime.now();
+  }
+
 }
+
+      
+
+
+    
+  
+
+
+
