98df9a0f78d01260fd767bb6f73a71017e8364c6
Haylee <hpollock3@gmail.com>
2023-10-16 18:04:28 -0400

    change Haylee

--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-// import 'package:flutter/material.dart';
-
-// class OrderPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       title: 'OrderPage',
-//       home: const OrderPage(),
-//     );
-//   }
-// }
-
-// class OrderPage extends StatefulWidget {
-//   const OrderPage({super.key});
-
-//   @override
-//   _OrderPageState createState() => _OrderPageState();
-// }
-
-// class _OrderPageState extends State<OrderPage> {
-//   final List<String> items = [
-//     'Entree', 
-//     'Sides',
-//     'Fruit',
-//     'Dessert',
-//     'Drinks'
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Start New Order'),
-//       ),
-//       body: ListView.builder(
-//         itemCount: items.length,
-//         itemBuilder: (BuildContext context) {
-//           return ListTile(
-//             leading: Icon(
-//               Icons.circle,
-//             ),
-//             title: Text(items[index]),
-//             // subtitle: Text(''),
-//             trailing: const Icon(Icons.arrow_forward),
-//             onTap: () {}
-//           )
-//         }
-//       )
-//     )
-//   }
-
-// }
-=======
 import 'package:flutter/material.dart';
 import 'widgets/HeaderBar.dart';
 import 'widgets/BottomBar.dart';
@@ -91,4 +37,3 @@ class OrderPage extends StatelessWidget {
           )
         }
       ), */
->>>>>>> b0f49caad40808a0c94ca40284bf3c5ca0200891
