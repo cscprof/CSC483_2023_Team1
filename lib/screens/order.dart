4a7929a3931b41848263aeada9f1438f6327eae7
amvolpe <amvolpe@geneva.edu>
2023-10-16 21:01:09 -0400

    order page update

--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'widgets/HeaderBar.dart';
 import 'widgets/BottomBar.dart';
 
+final List<String> items = <String>['Entrees', 'Sides', 'Fruit', 'Dessert', 'Drinks'];
+// final List<int> colorCodes = <int>[600, 500, 400, 300, 200];
+
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
 
@@ -10,31 +13,40 @@ class OrderPage extends StatelessWidget {
     return MaterialApp(
       title: "Order Page",
       home: Scaffold(
-        appBar: TopAppBar(AppBar(), "Start New Order", Colors.yellow, Colors.black),
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(10.0),
-          children: [],
-        ),
-        ),
+        appBar: TopAppBar(AppBar(), "New Order", Colors.yellow, Colors.black),
+        body: const Column(children: [Expanded(flex: 1, child: ListViewBuilder()),],),
         bottomNavigationBar: BottomBar(AppBar(), WhichPage.home),
       )
       );
   }
 }
 
+class ListViewBuilder extends StatelessWidget {
+  const ListViewBuilder({super.key});
 
-/* ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (BuildContext context) {
-          return ListTile(
-            leading: Icon(
-              Icons.circle,
-            ),
-            title: Text(items[index]),
-            // subtitle: Text(''),
-            trailing: const Icon(Icons.arrow_forward),
-            onTap: () {}
-          )
-        }
-      ), */
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return SizedBox(
+          height: 60,
+          // color: Colors.amber[colorCodes[index]],
+          child: TextButton(
+            onPressed: () {}, 
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(items[index], style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),),
+                const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black,)
+              ],
+            )
+            )
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      );
+  }
+}
+
