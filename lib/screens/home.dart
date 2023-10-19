75147e022bdfde0e559022dc15e2ea9ca96d85ba
Zoe Pifer <znpifer@geneva.edu>
2023-10-18 20:36:47 -0400

    images folder + home changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,7 @@ import 'widgets/HeaderBar.dart';
 import 'widgets/BottomBar.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,59 +12,90 @@ class HomePage extends StatelessWidget {
       title: 'Mobile Order Page',
       home: Scaffold(
         appBar: TopAppBar(AppBar(), "Home Page", Colors.yellow, Colors.black),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Welcome to our mobile order page!',
-                style: TextStyle(fontSize: 20),
+        body: Stack( // Use a Stack to overlay images
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/backgroundColor.png"), // Background image
+                  fit: BoxFit.cover,
+                ),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Display a dialog with two buttons: New Order and Reorder
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Order Options'),
-                        content: const Text('Choose an option:'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              // Close the dialog
-                              Navigator.of(context).pop();
+            ),
+            Positioned(
+              top: 100, // Adjust the position as needed
+              left: 50, // Adjust the position as needed
+              child: Image.asset(
+                "images/tornadoTransparent.png", // First overlay image
+                width: 700, // Adjust the width as needed
+                height: 700, // Adjust the height as needed
+            
+              ),
+            ),
+            Positioned(
+              top: 200, // Adjust the position as needed
+              left: 150, // Adjust the position as needed
+              child: Image.asset(
+                "images/wordBrig.png", // Second overlay image
+                width: 150, // Adjust the width as needed
+                height: 150, // Adjust the height as needed
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Welcome to our mobile order page!',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Display a dialog with two buttons: New Order and Reorder
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Order Options'),
+                            content: const Text('Choose an option:'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  // Close the dialog
+                                  Navigator.of(context).pop();
                               
-                              // Add logic for creating a new order here
-                              // Navigate to a new order screen
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => LoginPage()),
-                              );
-                            },
-                            child: const Text('New Order'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              // Close the dialog
-                              Navigator.of(context).pop();
+                                  // Add logic for creating a new order here
+                                  // Navigate to a new order screen
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => LoginPage()),
+                                  );
+                                },
+                                child: const Text('New Order'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  // Close the dialog
+                                  Navigator.of(context).pop();
                               
-                              // Add logic for reordering here
-                              // For example, repeat the previous order
-                            },
-                            child: const Text('Reorder'),
-                          ),
-                        ],
+                                  // Add logic for reordering here
+                                  // For example, repeat the previous order
+                                },
+                                child: const Text('Reorder'),
+                              ),
+                            ],
+                          );
+                        },
                       );
                     },
-                  );
-                },
-                child: const Text('Place an Order'),
+                    child: const Text('Place an Order'),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ), 
+            ),
+          ],
+        ),
         bottomNavigationBar: BottomBar(AppBar(), WhichPage.home),
       ),
     );
