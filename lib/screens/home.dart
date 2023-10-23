d0393d182757e1098432823237e4735eed63904f
Zoe Pifer <znpifer@geneva.edu>
2023-10-23 11:04:05 -0400

    picture adjustment

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'settings.dart'; // Import the Order.dart file
 import 'widgets/HeaderBar.dart';
 import 'widgets/BottomBar.dart';
 
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -12,84 +13,56 @@ class HomePage extends StatelessWidget {
       title: 'Mobile Order Page',
       home: Scaffold(
         appBar: TopAppBar(AppBar(), "Home Page", Colors.yellow, Colors.black),
-        body: Stack( // Use a Stack to overlay images
+        body: Stack(
           children: [
             Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("images/backgroundColor.png"), // Background image
+                  image: AssetImage("images/backgroundColor.png"),
                   fit: BoxFit.cover,
                 ),
               ),
             ),
             Positioned(
-              top: 100, // Adjust the position as needed
-              left: 50, // Adjust the position as needed
+              top: 20,
+              left: MediaQuery.of(context).size.width / 3,
               child: Image.asset(
-                "images/tornadoTransparent.png", // First overlay image
-                width: 700, // Adjust the width as needed
-                height: 700, // Adjust the height as needed
-            
+                "images/tornadoTransparent.png",
+                width: 400,
+                height: 700,
               ),
             ),
             Positioned(
-              top: 200, // Adjust the position as needed
-              left: 150, // Adjust the position as needed
+              top: 200,
+              left: MediaQuery.of(context).size.width / 5,
               child: Image.asset(
-                "images/wordBrig.png", // Second overlay image
-                width: 150, // Adjust the width as needed
-                height: 150, // Adjust the height as needed
+                "images/wordBrig.png",
+                width: 700,
+                height: 500,
               ),
             ),
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text(
-                    'Welcome to our mobile order page!',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: () {
-                      // Display a dialog with two buttons: New Order and Reorder
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Order Options'),
-                            content: const Text('Choose an option:'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  // Close the dialog
-                                  Navigator.of(context).pop();
-                              
-                                  // Add logic for creating a new order here
-                                  // Navigate to a new order screen
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => SettingsPage()),
-                                  );
-                                },
-                                child: const Text('New Order'),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  // Close the dialog
-                                  Navigator.of(context).pop();
-                              
-                                  // Add logic for reordering here
-                                  // For example, repeat the previous order
-                                },
-                                child: const Text('Reorder'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
+                      // Add your logic for the "New Order" button here
                     },
-                    child: const Text('Place an Order'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black), // Set the background color to black
+                    ),
+                    child: const Text('New Order', style: TextStyle(color: Colors.white)),
+                  ),
+                  const SizedBox(height: 10), // Add some spacing between the buttons
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your logic for the "Reorder" button here
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black), // Set the background color to black
+                    ),
+                    child: const Text('Re Order', style: TextStyle(color: Colors.white)),
                   ),
                 ],
               ),
