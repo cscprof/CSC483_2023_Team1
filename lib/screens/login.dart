ea2dc74f658a8f6d164d8a2d6d65896bbf220f75
Haylee Pollock <hpollock@Haylees-MacBook-Air.local>
2024-02-29 10:53:38 -0500

    Hay 2.0

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -109,6 +109,7 @@ class _LoginPageState extends State<LoginPage> {
                  {  
                   // ignore: use_build_context_synchronously
                   Navigator.push(
+                    // ignore: use_build_context_synchronously
                     context, 
                    MaterialPageRoute(builder: (context) => const HomePage())
                            );
